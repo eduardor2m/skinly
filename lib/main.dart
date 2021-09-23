c97b0ce1a73e1759e162ab50274524b67ef7d8b4
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-09-22 21:33:30 -0300

    Create avatar informations screen initial structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:skinly/screens/models.dart';
+import 'package:skinly/screens/choose_model.dart';
 import 'package:skinly/screens/export.dart';
 import 'package:skinly/screens/my_avatars.dart';
 import 'package:skinly/screens/splash.dart';
 import 'package:skinly/screens/welcome.dart';
+import 'package:skinly/screens/avatar_informations.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: '/splash',
+      initialRoute: '/models',
       routes: {
         '/splash': (context) => SplashScreen(),
         '/welcome': (context) => WelcomeScreen(),
         '/my-avatars': (context) => MyAvatarsScreen(),
-        '/models': (context) => ModelsScreen(),
+        '/choose-model': (context) => ModelsScreen(),
         '/export': (context) => ExportScreen(),
+        '/avatar-informations': (context) => AvatarInformationsScreen(),
       },
     ),
   );

129f75d9db7c95c5d6cdd5bfd83b709bf1e9a6a1
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-10-24 13:55:31 -0300

    Update input widget

--- lib/widgets/input_widget.dart
+++ lib/widgets/input_widget.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 class InputWidget extends StatelessWidget {
   final String label;
+  final Function(String)? onChange;
   
-  const InputWidget({Key? key, required this.label}) : super(key: key);
+  const InputWidget({
+    required this.label,
+    this.onChange,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      onChanged: (String newValue) => onChange!(newValue),
       decoration: InputDecoration(
         labelText: label,
         border: OutlineInputBorder(),

fc71bea1a374a7eae34548596531a67a87e68274
Filipe Alves <filipe.alvesdsouza@gmail.com>
2021-12-27 13:37:31 -0300

    Add controller and validator

--- lib/widgets/input_widget.dart
+++ lib/widgets/input_widget.dart
@@ -2,18 +2,25 @@ import 'package:flutter/material.dart';
 
 class InputWidget extends StatelessWidget {
   final String label;
-  final Function(String)? onChange;
+  final TextEditingController? controller;
   
   const InputWidget({
     required this.label,
-    this.onChange,
+    this.controller,
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      onChanged: (String newValue) => onChange!(newValue),
+      controller: controller,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Por favor, preencha esse campo';
+        }
+
+        return null;
+      },
       decoration: InputDecoration(
         labelText: label,
         border: OutlineInputBorder(),
